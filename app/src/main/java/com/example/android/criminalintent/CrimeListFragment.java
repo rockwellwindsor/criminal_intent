ed84d321a37b257faedf798902371e1feac3ed2b
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-02-21 13:53:56 -0600

    Development
    Changed the toast on list item click to load CrimeActivity.
    
    Changes to be committed:
        modified:   app/src/main/java/com/example/android/criminalintent/CrimeListFragment.java

--- app/src/main/java/com/example/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/android/criminalintent/CrimeListFragment.java
@@ -4,6 +4,7 @@ package com.example.android.criminalintent;
  * Created by rockwellrice on 2/13/17.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,7 +14,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -71,9 +71,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity(),
-                    mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                    .show();
+            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            startActivity(intent);
         }
     }
 
