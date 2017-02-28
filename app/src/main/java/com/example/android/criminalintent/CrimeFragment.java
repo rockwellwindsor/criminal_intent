4ffba1ef8a06261b669e4a23d62d1ad7c6febfc2
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-02-28 11:31:30 -0600

    Development
    Displaying a calendar view in the date picker.  Added the layout, and then inflate and call it in the DatPickerFragment.java file.
    
    Changes to be committed:
        modified:   app/src/main/java/com/example/android/criminalintent/CrimeFragment.java
        modified:   app/src/main/java/com/example/android/criminalintent/DatePickerFragment.java
        new file:   app/src/main/res/layout/dialog_date.xml

--- app/src/main/java/com/example/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/android/criminalintent/CrimeFragment.java
@@ -3,6 +3,7 @@ package com.example.android.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -18,6 +19,8 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
@@ -67,7 +70,14 @@ public class CrimeFragment extends Fragment {
         mDate = (df.format("MM-dd-yyyy hh:mm:ss a", new java.util.Date())).toString();
         mDateButton = (Button)v.findViewById(R.id.crime_date);
         mDateButton.setText(mDate);
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
--- app/src/main/java/com/example/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/example/android/criminalintent/DatePickerFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by rockwellrice on 2/28/17.
@@ -12,6 +14,7 @@ import android.support.v7.app.AlertDialog;
 public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity()).setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null).create();
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+        return new AlertDialog.Builder(getActivity()).setView(v).setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null).create();
     }
 }
